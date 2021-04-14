4e1476a570d4f0ffe2080132374294f910e658f2
Avery Cameron <35551198+AveryCameronUofR@users.noreply.github.com>
2021-04-14 14:58:18 -0600

    Cleaned up input splitting

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,8 +5,7 @@ export default Controller.extend({
   actions: {
     searchWeatherForecast(search) {
       let inputs =['',''];
-      inputs = search.split(',');
-      inputs = inputs.map(inputParam=>(inputParam.trim()));
+      inputs = search.split(',').map(inputParam=>(inputParam.trim()));
       this.store.queryRecord('3hourly', {city:inputs[0], country:inputs[1]}).then((weather) => {
         console.log(weather);
       });
