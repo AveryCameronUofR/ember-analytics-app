71ad6204735257e345b3d10fb96397f1c3994bd4
Avery Cameron <35551198+AveryCameronUofR@users.noreply.github.com>
2021-04-14 14:57:20 -0600

    Added string parsing for city and country

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,8 +3,11 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   
   actions: {
-    searchWeatherForecast(city) {
-      this.store.queryRecord('3hourly', {city:city}).then((weather) => {
+    searchWeatherForecast(search) {
+      let inputs =['',''];
+      inputs = search.split(',');
+      inputs = inputs.map(inputParam=>(inputParam.trim()));
+      this.store.queryRecord('3hourly', {city:inputs[0], country:inputs[1]}).then((weather) => {
         console.log(weather);
       });
     },
--- app/models/3hourly.js
+++ app/models/3hourly.js
@@ -1,8 +1,9 @@
 import DS from 'ember-data';
 export default DS.Model.extend({
-    city_name: DS.attr('string'),
-    state_code: DS.attr('string'),
-    country_code: DS.attr('string'),
-    lat: DS.attr('string'),
-    lon: DS.attr('string'),
+    data: {
+        city_name: DS.attr('string'),
+        country_code: DS.attr('string'),
+        lat: DS.attr('string'),
+        lon: DS.attr('string'),
+    }
 });
