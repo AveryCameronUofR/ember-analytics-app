dc7c25f202021bf3a132fcf9488b11fcc8800c98
Avery Cameron <35551198+AveryCameronUofR@users.noreply.github.com>
2021-04-15 09:23:46 -0600

    Updated sample api key

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,7 +5,7 @@ export default DS.RESTAdapter.extend({
     host: 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast',
     headers: computed(function() {
         return {
-            "x-rapidapi-key": "d82f81da83msh4bcbc76a26225f9p16316ejsn7f9d90c37f72",
+            "x-rapidapi-key": "0f7ba40775mshf71b39e0d5d52b4p173c04jsn457a2205e0a4",
             "x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
             "useQueryString": true
         };
