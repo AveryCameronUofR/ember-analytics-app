6a016eede5b86c149bd61e7e14c8ec3a8819e9d5
Avery Cameron <35551198+AveryCameronUofR@users.noreply.github.com>
2021-04-14 23:23:07 -0600

    Update index.js

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,14 +3,12 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   
   actions: {
-    async searchWeatherForecast(search) {
+    searchWeatherForecast(search) {
       let inputs =['',''];
       inputs = search.split(',').map(inputParam=>(inputParam.trim()));
-      let weather = await this.store.queryRecord('3hourly', {city:inputs[0], country:inputs[1]}).then(function(data){
-        console.log(data);
+      this.store.queryRecord('3hourly', {city:inputs[0], country:inputs[1]}).then(function(data){
+        data.json();
       });
-      console.log(weather);
-      return weather;
     },
   },
 });
